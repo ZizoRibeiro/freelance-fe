118f93575025359268d710421c94760f63be4587
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-21 17:34:54 -0800

    added document interface

---
+++ app/documents/document.js
@@ -0,0 +1,9 @@
+"use strict";
+file;
+url: string,
+    updated_at;
+string,
+    image_url;
+string,
+;
+//# sourceMappingURL=document.js.map
---
+++ app/documents/document.ts
@@ -0,0 +1,7 @@
+export interface Document {
+  title: string, 
+  description: string,
+  file url: string,
+  updated_at: string,
+  image_url: string, 
+}
