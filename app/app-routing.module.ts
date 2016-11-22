b1e61dc3d2ff5156dd237aff81c7261e83e9ac17
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-21 16:34:56 -0800

    fixed app routing module and created homepage componenent html

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   { path: 'home', component: HomepageComponent },
 ]
 
-@ngModule({
-  imports: [ RouterModule, forRoot(routes) ],
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule]
 })
 export class AppRoutingModule {}
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app',
-  template: `
+  templateUrl: 'app.component.html'
 
-  <h1>Navigation</h1>
-
-  <router-outlet></router-outlet>
-  `
 })
 export class AppComponent {
   title: 'Dashboard';
