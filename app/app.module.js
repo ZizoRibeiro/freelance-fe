40ed6c8d50a1efe6fe471b7171fb683113e02161
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-21 16:52:08 -0800

    created documents components

--- app/app.module.js
+++ app/app.module.js
@@ -12,6 +12,7 @@ var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
 var homepage_component_1 = require('./homepage/homepage.component');
+var documents_component_1 = require('./documents/documents.component');
 var app_routing_module_1 = require('./app-routing.module');
 var AppModule = (function () {
     function AppModule() {
@@ -24,7 +25,8 @@ var AppModule = (function () {
             ],
             declarations: [
                 homepage_component_1.HomepageComponent,
-                app_component_1.AppComponent
+                app_component_1.AppComponent,
+                documents_component_1.DocumentsComponent
             ],
             bootstrap: [
                 app_component_1.AppComponent
