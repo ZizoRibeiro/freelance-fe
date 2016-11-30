8e83fb326296a43df6b83bacc2201d70c4a37957
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-30 14:49:25 -0800

    Rendered the API data to the frontend .

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,36 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from './document';
+import { DocumentService } from './document.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls:['documents.component.css']
+  styleUrls:['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit{
   pageTitle: string = "Document Dashboard"
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable"; 
 
-  documents: Document[] = [
-    {
-      title: "My first Doc",
-      description: 'This doc was build to maintain...',
-      file_url: 'http://google.com',
-      updated_at: '12/11/16',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    },
-    {
-      title: "My second Doc",
-      description: 'This doc was build to maintain...',
-      file_url: 'http://google.com',
-      updated_at: '12/11/16',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    },
-    {
-      title: "My last Doc",
-      description: 'This doc was build to maintain...',
-      file_url: 'http://google.com',
-      updated_at: '12/11/16',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    }
-  ]
+  constructor(
+    private documentService: DocumentService;
+    ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+          );
+  }
+     
 }
